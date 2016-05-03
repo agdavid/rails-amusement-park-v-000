ba2652e7db949676901157b2c1c526a19076914f
agdavid <antonio.g.david@gmail.com>
2016-05-03 19:28:15 -0400

    model tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
   # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    #sad if nausea > happiness
+    #happy if happiness > nausea
+    self.happiness > self.nausea ? "happy" : "sad"
+  end
+
 end
