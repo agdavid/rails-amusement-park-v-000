3718df350a301fb4cbf3193aa2e6e39aedb826ca
agdavid <antonio.g.david@gmail.com>
2016-05-04 15:01:05 -0400

    passing tests for features admin flow

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,5 +8,36 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.find(params[:id])
     @ride = Ride.new
   end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to attraction_path(@attraction), :notice => "Attraction was successfully created."
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    if @attraction.update_attributes(attraction_params)
+      redirect_to attraction_path(@attraction), :notice => "Attraction was successfully updated."
+    else
+      render "edit"
+    end
+  end
+
+  private
+    def attraction_params
+      params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+    end
   
 end
