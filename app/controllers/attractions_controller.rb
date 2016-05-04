cbe87d3321b4054a1a0dcdf8cdc5ddaa63c4edc6
agdavid <antonio.g.david@gmail.com>
2016-05-04 15:09:06 -0400

    refactor repeat edit/new forms into partials

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,11 +1,11 @@
 class AttractionsController < ApplicationController
+  before_action :set_attraction, only: [:show, :edit, :update]
 
   def index
     @attractions = Attraction.all
   end
 
   def show
-    @attraction = Attraction.find(params[:id])
     @ride = Ride.new
   end
 
@@ -23,11 +23,9 @@ class AttractionsController < ApplicationController
   end
 
   def edit
-    @attraction = Attraction.find(params[:id])
   end
 
   def update
-    @attraction = Attraction.find(params[:id])
     if @attraction.update_attributes(attraction_params)
       redirect_to attraction_path(@attraction), :notice => "Attraction was successfully updated."
     else
@@ -36,6 +34,10 @@ class AttractionsController < ApplicationController
   end
 
   private
+    def set_attraction
+      @attraction = Attraction.find(params[:id])  
+    end
+
     def attraction_params
       params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(id: params[:user][:id])
+    @user = User.find(params[:user][:id])
     if @user
       session[:user_id] = @user.id
       redirect_to user_path(@user)
