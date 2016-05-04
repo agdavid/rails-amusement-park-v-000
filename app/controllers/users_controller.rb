ae4e5402f194371bf1d835692410f0bbef53994a
agdavid <antonio.g.david@gmail.com>
2016-05-03 23:20:49 -0400

    feature tests: signup passing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :admin)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
+  root "welcome#index"
 
+  resources :users
+  resources :sessions, only: [:create]
+
+  get "/signin" => "sessions#new"
+  
 end
